73fb7c4ae5df3f665bcad6674be143ff32266c71
Fabian Oehlmann <fabian.oehlmann@jambit.com>
2018-12-09 23:53:36 +0100

    [Day09] Solution to part two

--- src/main/scala/aoc/day09/MarbleMania.scala
+++ src/main/scala/aoc/day09/MarbleMania.scala
@@ -7,46 +7,46 @@ object MarbleMania extends App {
   val inputQtyPlayers: Int = 478
   val inputQtyMarbles: Int = 71240
 
-  case class Elf(id: Int, score: Int)
+  case class Elf(id: Int, score: Long)
 
-  case class GameState(circle: Seq[Int],
+  case class GameState(circle: Vector[Int],
                        remainingMarbles: Stream[Int],
                        elves: Seq[Elf],
                        curElf: Int)
 
   def initialState(qtyMarbles: Int, qtyPlayers: Int) = GameState(
-    Seq(0),
+    Vector(0),
     (1 to qtyMarbles).toStream,
     (1 to qtyPlayers).map(Elf(_, 0)),
     0
   )
 
-  val nextMarble: State[GameState, Int] = State[GameState, Int]( s =>
+  val nextMarble: State[GameState, Int] = State[GameState, Int](s =>
     (GameState(s.circle, s.remainingMarbles.drop(1), s.elves, s.curElf), s.remainingMarbles.head)
   )
 
-  val nextElf: State[GameState, Elf] = State[GameState, Elf]( s => {
+  val nextElf: State[GameState, Elf] = State[GameState, Elf](s => {
     val elfId = (s.curElf % inputQtyPlayers) + 1
     (GameState(s.circle, s.remainingMarbles, s.elves, elfId), s.elves.find(_.id == elfId).get)
   })
 
-  def rotateRight(is: Seq[Int], n: Int): Seq[Int] = {
+  def rotateRight(is: Vector[Int], n: Int): Vector[Int] = {
     val length = is.length
     val shift = length - (n % length)
     is.drop(shift) ++ is.take(shift)
   }
 
-  def rotateLeft(is: Seq[Int], n: Int): Seq[Int] = {
+  def rotateLeft(is: Vector[Int], n: Int): Vector[Int] = {
     val shift = n % is.length
     is.drop(shift) ++ is.take(shift)
   }
 
-  def placeMarble(marble: Int): State[GameState, Unit] = State[GameState, Unit]( s => {
+  def placeMarble(marble: Int): State[GameState, Unit] = State[GameState, Unit](s => {
     val nextCircle = marble +: rotateLeft(s.circle, 2)
     (GameState(nextCircle, s.remainingMarbles, s.elves, s.curElf), Unit)
   })
 
-  val removeMarble: State[GameState, Int] = State[GameState, Int]( s => {
+  val removeMarble: State[GameState, Int] = State[GameState, Int](s => {
     val rotatedCircle = rotateRight(s.circle, 7)
     (GameState(rotatedCircle.tail, s.remainingMarbles, s.elves, s.curElf), rotatedCircle.head)
   })
@@ -86,8 +86,15 @@ object MarbleMania extends App {
       })
   }
 
-  val winningElfScore = gameLoop.runS(initialState(inputQtyMarbles, inputQtyPlayers)).value.elves.maxBy(_.score).score
+  val winningElfScore = gameLoop.runS(initialState(inputQtyMarbles, inputQtyPlayers))
+    .value.elves.maxBy(_.score).score
 
   println(s"What is the winning Elf's score? $winningElfScore")
+
+  val winningElfScore100 = gameLoop.runS(initialState(inputQtyMarbles * 100, inputQtyPlayers))
+    .value.elves.maxBy(_.score).score
+
+  println(s"What would the new winning Elf's score be if the number of the last marble were 100 times larger? " +
+    s"$winningElfScore100")
 
 }
